499dd2817c17a34b4992069301c77d150ae14d13
romanov.da <dimromgomell@gmail.com>
2026-04-12 13:27:04 +0300

    simple echo-server

---
+++ 1_echo_server.py
@@ -0,0 +1,36 @@
+import asyncio
+import socket
+from asyncio import AbstractEventLoop
+
+
+async def echo(connection: socket, loop: AbstractEventLoop) -> None:
+    while data := await loop.sock_recv(connection, 1024):
+        await loop.sock_sendall(connection, data)
+
+async def listen_for_connection(
+        server_socket: socket,
+        loop: AbstractEventLoop
+):
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f"Получен запрос на подключение от {address}")
+        asyncio.create_task(echo(connection, loop))
+
+async def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    await listen_for_connection(
+        server_socket = server_socket,
+        loop = asyncio.get_event_loop()
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
