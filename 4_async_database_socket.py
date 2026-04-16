64f57ce9e1602dd8538999e8d8dd32c14024adc6
romanov.da <dimromgomell@gmail.com>
2026-04-16 23:25:19 +0300

    learning how to create connection pool; time complexity testing

--- 4_async_database_socket.py
+++ 4_async_database_socket.py
@@ -1,58 +1,46 @@
+from functools import wraps
+import time
+
 import asyncpg
 import asyncio
-
-from random import sample, randint
 
 from sql_commands import *
 
 
-def load_common_words() -> list[str]:
-    with open("common_words.txt") as common_words:
-        return common_words.readlines()
-    
-def generate_brand_names(words: list[str]):
-    return [(words[index], ) for index in sample(range(100), 100)]
+query = "INSERT INTO test_table (test_val) VALUES (52)";
 
-async def insert_brand_names(common_words, connection) -> int:
-    brands = generate_brand_names(common_words)
-    insert_brands = "INSERT INTO brand VALUES(DEFAULT, $1)"
-    return await connection.executemany(insert_brands, brands)
+def async_timed(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = await func(*args, **kwargs)
+        duration = time.perf_counter() - start
+        print("Время выполенния: ", duration)
+        return result
+    return wrapper
 
-def gen_products(
-    common_words: list[str],
-    brand_id_start: int,
-    brand_id_end: int,
-    products_to_create: int
-) -> list[tuple[str, int]]:
-    products = []
-    for _ in range(products_to_create):
-        description = [common_words[index] for index in sample(range(1000), 10)]
-        brand_id = randint(brand_id_start, brand_id_end)
-        products.append((" ".join(description), brand_id))
-    return products
 
-def gen_skus(product_id_start: int,
-    product_id_end: int,
-    skus_to_create: int) -> list[tuple[int, int, int]]:
-    skus = []
-    for _ in range(skus_to_create):
-        product_id = randint(product_id_start, product_id_end)
-        size_id = randint(1, 3)
-        color_id = randint(1, 2)
-        skus.append((product_id, size_id, color_id))
-    return skus
+async def query_product(pool):
+    async with pool.acquire() as connection:
+        return await connection.fetch(query)
 
+@async_timed
 async def main():
-    common_words = load_common_words()
-    connection = await asyncpg.connect(
-        host = "0.0.0.0",
-        port = 5432,
-        user = "asyncio_book_user",
-        database = "asyncio_book",
-        password = "12345678"
-    )
-
-
-    await connection.close()
+    async with asyncpg.create_pool(
+                        host = "0.0.0.0",
+                        port = 5432,
+                        user = "asyncio_book_user",
+                        database = "asyncio_book",
+                        password = "12345678",
+                        min_size=6,
+                        max_size=6
+                    ) as pool:
+        
+        fetches = [await query_product(pool) for _ in range(10000)]
+        # fetches = [query_product(pool) for _ in range(10000)]
+        # await asyncio.gather(*fetches)
+    
 
 asyncio.run(main())
+
+# 0.10414658800436882
