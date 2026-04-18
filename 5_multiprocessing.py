e95d490185d6a5b3055d2e9daca28b84717fa698
romanov.da <dimromgomell@gmail.com>
2026-04-18 21:26:10 +0300

    learning about sync methods

--- 5_multiprocessing.py
+++ 5_multiprocessing.py
@@ -1,34 +1,23 @@
-from asyncio import AbstractEventLoop
 import asyncio
-from functools import partial
-import time
+from multiprocessing import Value
 from concurrent.futures import ProcessPoolExecutor
 
+shared_count = Value("d", 0)
 
-def count(count_to: int) -> int:
-    start = time.perf_counter()
-    counter = 0
-    while counter < count_to:
-        counter += 1
-    duration = time.perf_counter() - start
-    print("Время выполенения: ", duration)
-    return counter
+def init(counter):
+    global shared_count
+    shared_count = counter
 
+def increment():
+    with shared_count.get_lock():
+        shared_count.value += 1
 
 async def main():
-    with ProcessPoolExecutor() as process_pool:
-        loop: AbstractEventLoop = asyncio.get_running_loop()
-        nums = [100_000_000, 1, 2, 5, 22, 100_000_000]
-        calls: list[partial[int]] = [partial(count, num) for num in nums]
-        calls_coros = []
+    counter = Value("d", 0)
+    with ProcessPoolExecutor(initializer=init, initargs=(counter, )) as pool:
+        await asyncio.get_running_loop().run_in_executor(pool, increment)
+        print(counter.value)
 
-        for call in calls:
-            calls_coros.append(loop.run_in_executor(process_pool, call))
-
-        results = await asyncio.gather(*calls_coros)
-
-        for res in results:
-            print(res)
 
 if __name__ == "__main__":
     asyncio.run(main())
