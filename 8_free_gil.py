6a6a34584772ccbaca94aff1f139a3a2b269bb25
romanov.da <dimromgomell@gmail.com>
2026-04-22 23:12:44 +0300

    example with threads (5 times faster)

--- 8_free_gil.py
+++ 8_free_gil.py
@@ -3,13 +3,32 @@ import os
 import string
 import random
 import time
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from typing import Callable, Any, Coroutine
+from functools import partial, wraps
+
+
+def async_timer[R, **P](
+    func: Callable[P, Coroutine[Any, Any, R]]
+) -> Callable[P, Coroutine[Any, Any, R]]:
+    
+    @wraps(func)
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        start = time.perf_counter()
+        try:
+            return await func(*args, **kwargs)
+        finally:
+            duration = time.perf_counter() - start
+            print("duration: ", duration)
+
+    return wrapper
 
 
 def get_random_password(length: int) -> bytes:
     letters = string.ascii_letters.encode()
     return b"".join(bytes(random.choice(letters)) for _ in range(length))
 
-passwords = [get_random_password(10) for _ in range(10_000)]
 
 def hash(password: bytes) -> str:
     salt = os.urandom(16)
@@ -21,10 +40,21 @@ def hash(password: bytes) -> str:
         r = 8 
     )
 
-start = time.time()
+@async_timer
+async def main():
+    with ThreadPoolExecutor() as pool:
+        loop = asyncio.get_running_loop()
+        passwords = [get_random_password(10) for _ in range(10_000)]
+        tasks = [
+            loop.run_in_executor(
+                executor=pool,
+                func = partial(hash, password),
+            )
+            for password in passwords
+        ]
+        await asyncio.gather(*tasks)
 
-for password in passwords:
-    hash(password)
 
-end = time.time()
-print(end - start)
+asyncio.run(main())
+
+        
