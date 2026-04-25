4fc86b857872e74cfb3614343825831071afa8f2
romanov.da <dimromgomell@gmail.com>
2026-04-25 19:53:41 +0300

    hello world in aiohttp

---
+++ 10_aiohttp_rest.py
@@ -0,0 +1,24 @@
+from aiohttp import web
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+
+from datetime import datetime
+
+
+routes = web.RouteTableDef()
+
+@routes.get("/time")
+async def time(request: Request) -> Response:
+    today = datetime.today()
+    result = {
+        "month": today.month,
+        "day": today.day,
+        "time": str(today.time())
+    }
+    return web.json_response(result)
+
+app = web.Application()
+
+app.add_routes(routes)
+
+web.run_app(app)
